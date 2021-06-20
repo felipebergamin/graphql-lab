34b9285045c57ec809d3308e0778e6f469aeb9fe
Felipe Bergamin <felipebergamin6@gmail.com>
2021-06-19 22:07:07 -0300

    feat(backend): implement user migration and entity

---
+++ packages/backend/src/entity/User.ts
@@ -0,0 +1,60 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+    BeforeInsert,
+    BeforeUpdate,
+} from "typeorm";
+import { hash, compare } from 'bcryptjs';
+
+@Entity({
+    name: 'users'
+})
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    id!: string;
+
+    @Column()
+    firstName!: string;
+
+    @Column()
+    lastName!: string;
+
+    @Column()
+    email!: string;
+
+    @Column({ select: false })
+    password!: string;
+
+    plainTextPassword!: string | undefined;
+
+    @CreateDateColumn({
+        type: 'timestamp with time zone'
+    })
+    createdAt!: Date;
+
+    @UpdateDateColumn({
+        type: 'timestamp with time zone'
+    })
+    updatedAt!: Date;
+
+    @BeforeInsert()
+    async hashPassword() {
+        console.log('beforeInsert', this.plainTextPassword, this.email);
+        if (!this.plainTextPassword) throw new Error('No password provided');
+        this.password = await hash(this.plainTextPassword, 10);
+    }
+
+    @BeforeUpdate()
+    async updatePassword() {
+        if (this.plainTextPassword) {
+            this.password = await hash(this.plainTextPassword, 10);
+        }
+    }
+
+    checkPassword(plainPassword: string): Promise<boolean> {
+        return compare(plainPassword, this.password);
+    }
+}
