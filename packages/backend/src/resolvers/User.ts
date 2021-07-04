33eab6c44c680b6b17ced38feecec284d4223173
Felipe Bergamin <felipebergamin6@gmail.com>
2021-07-04 12:11:24 -0300

    refactor(backend): implement new user resolvers

--- packages/backend/src/resolvers/User.ts
+++ packages/backend/src/resolvers/User.ts
@@ -4,14 +4,36 @@ import { getCustomRepository } from 'typeorm';
 import UserRepository from '../repositories/User';
 
 const UserResolvers: Resolvers = {
+  User: {
+    passwords: (parent) => {
+      return parent.passwords;
+    },
+  },
   Query: {
-    users: () => getCustomRepository(UserRepository).find(),
+    me: (parent, args, context) => {
+      const repository = getCustomRepository(UserRepository);
+      return repository.findOneOrFail(context.user.id, {
+        relations: ['passwords'],
+      });
+    },
   },
   Mutation: {
     newUser: (parent, { input }) => {
       const repository = getCustomRepository(UserRepository);
       const user = repository.create(input);
-      user.plainTextPassword = input.plainTextPassword;
+      user.receiveNewPlainTextPassword(input.plainTextPassword);
+      return repository.save(user);
+    },
+    updateProfile: async (parent, { input }, context, info) => {
+      const repository = getCustomRepository(UserRepository);
+      const user = await repository.findOneOrFail(context.user.id);
+      const { email, plainTextPassword, firstName, lastName } = input;
+      if (email) user.email = email;
+      if (plainTextPassword) {
+        user.receiveNewPlainTextPassword(plainTextPassword);
+      }
+      if (firstName) user.firstName = firstName;
+      if (lastName) user.lastName = lastName;
       return repository.save(user);
     },
   },
